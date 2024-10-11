2e27358226c1f7ee3598ef918d8666fd876fba88
Eduard Kulikov <kulikov.e.ev@gmail.com>
2024-10-12 00:47:43 +0300

    notifications

---
+++ Source/RoastTemples/ClassForProjectBuild.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClassForProjectBuild.h"
+
+ClassForProjectBuild::ClassForProjectBuild()
+{
+}
+
+ClassForProjectBuild::~ClassForProjectBuild()
+{
+}
